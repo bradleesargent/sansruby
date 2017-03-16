7eb7a9144313dd3dec4689785f379e63d6fc73bf
Bradlee Sargent <bradlee.sargent@gmail.com>
2017-03-16 17:51:51 -0400

    add validation for length, releaseyear and title

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  validates :length, numericality: { less_than_or_equal_to: 500, message: " of movie must be less than 500 minutes long"}
+  validates :title, length: {maximum: 50 }
+  validates :releaseyear , numericality: { greater_than: 1800, less_than: 2100, message: " of movie must be between 1801 and 2099"}
 end
--- put.bat
+++ put.bat
@@ -5,6 +5,6 @@ REM git config --global user.name "Bradlee Sargent"
 REM git remote rm sansruby
 REM git init
 git add .
-git commit -m "added image and raw menu and input"
+git commit -m "add validation for length, releaseyear and title"
 REM git remote add sansruby https://github.com/bradleesargent/sansruby.git
 git push -u sansruby master
