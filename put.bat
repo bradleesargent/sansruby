3415da4bcae5a1e3828319e6cca4eb427775f599
Bradlee Sargent <bradlee.sargent@gmail.com>
2017-03-17 14:32:05 -0400

    Display length as hours and minutes on list page

--- put.bat
+++ put.bat
@@ -5,6 +5,6 @@ REM git config --global user.name "Bradlee Sargent"
 REM git remote rm sansruby
 REM git init
 git add .
-git commit -m "Changed rating to radio buttons"
+git commit -m "Display length as hours and minutes on list page"
 REM git remote add sansruby https://github.com/bradleesargent/sansruby.git
 git push -u sansruby master
